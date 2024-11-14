a338363ad47a4de7bd5a3838c6a7b9022539a11a
Jerome Wolff <hello@jeromewolff.de>
2024-11-14 11:48:32 +0100

    feat: Add vercel speed-insights

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {SpeedInsights} from "@vercel/speed-insights/react";
 import './styles/index.scss';
 
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<App/>);
+root.render(
+  <React.StrictMode>
+    <App/>
+    <SpeedInsights/>
+  </React.StrictMode>
+);
