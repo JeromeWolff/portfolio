974659eb4602c0ef52947a1ddd5f10a670530f35
Jerome Wolff <hello@jeromewolff.de>
2024-11-14 11:52:04 +0100

    feat: Add vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {SpeedInsights} from "@vercel/speed-insights/react";
+import {Analytics} from "@vercel/analytics/react";
 import './styles/index.scss';
 
 const rootElement = document.getElementById('root') as HTMLElement
@@ -11,5 +12,6 @@ root.render(
   <React.StrictMode>
     <App/>
     <SpeedInsights/>
+    <Analytics/>
   </React.StrictMode>
 );
