a6b98d76207db3dd62803220382cf2d49f0464c5
Jerome Wolff <hello@jeromewolff.de>
2025-05-07 17:08:49 +0200

    fix: Sentry initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
 import App from './App';
 import {SpeedInsights} from "@vercel/speed-insights/react";
 import {Analytics} from "@vercel/analytics/react";
-import {initSentry} from "./sentry";
 import './styles/index.scss';
 
-initSentry({
-  dsn: import.meta.env.SENTRY_DSN
+Sentry.init({
+  dsn: import.meta.env.SENTRY_DSN,
+  sendDefaultPii: true,
 });
 
 const rootElement = document.getElementById('root') as HTMLElement
