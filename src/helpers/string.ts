def0d7d276a31bf5caf31333ebc9c3f839535bad
Jerome Wolff <hello@jeromewolff.de>
2025-06-26 18:03:51 +0200

    refactor: Use any and disable @typescript-eslint/no-explicit-any rule for format function

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,4 +1,5 @@
-export function format(template: string, ...args: string[]): string {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function format(template: string, ...args: any[]): string {
   return template.replace(/{(\d+)}/g, (match, index) => {
     return typeof args[index] !== 'undefined' ? args[index] : match;
   });
