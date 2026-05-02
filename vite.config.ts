e40dde5bdda469fa07f74eeb52f3158515ad8bdb
Jerome Wolff <hello@jeromewolff.de>
2026-05-02 16:23:37 +0200

    chore(deps): Remove vite-tsconfig-paths and associated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,6 @@
-import * as fs from 'fs';
-import path from 'path';
-
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-function getPathsFromTsConfig(): Record<string, string> {
-  const tsconfigRaw = fs.readFileSync('./tsconfig.json', 'utf-8').replace(/\/\/.*$/gm, ''); // Removing comments
-  const tsconfig = JSON.parse(tsconfigRaw) as {
-    compilerOptions: { paths?: Record<string, string[]> };
-  };
-  const aliases: Record<string, string> = {};
-  const paths = tsconfig.compilerOptions.paths ?? {};
-  for (const [key, value] of Object.entries(paths)) {
-    if (Array.isArray(value) && typeof value[0] === 'string') {
-      // Handle both wildcard and base alias
-      if (key.endsWith('/*') && value[0].endsWith('/*')) {
-        aliases[key.replace('/*', '')] = path.resolve(__dirname, value[0].replace('/*', ''));
-        aliases[key] = path.resolve(__dirname, value[0]);
-      } else {
-        aliases[key] = path.resolve(__dirname, value[0]);
-      }
-    }
-  }
-  return aliases;
-}
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -53,11 +28,10 @@ export default defineConfig(({ mode }) => {
       'process.env.SENTRY_AUTH_TOKEN': JSON.stringify(env.SENTRY_AUTH_TOKEN),
     },
     resolve: {
-      alias: getPathsFromTsConfig(),
+      tsconfigPaths: true,
     },
     plugins: [
       react(),
-      tsconfigPaths(),
       sentryVitePlugin({
         org: 'jerome-wolff',
         project: 'portfolio',
